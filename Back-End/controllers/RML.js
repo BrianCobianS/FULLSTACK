be77664c943e536775c3d82f6641bb101e2351cf
brian.cobian <brian.cobian@toshibagcs.com>
2022-11-30 16:05:38 -0600

    updatete for RPML

---
+++ Back-End/controllers/RML.js
@@ -0,0 +1,59 @@
+
+import { exec } from 'child_process';
+
+const RML = (req,res) =>{
+    const {checkcontroller} = req.body
+    console.log(checkcontroller)
+    console.log(checkcontroller.ip)
+    exec(`sh /home/ebossteam/RML/ojjo/excecuteme.sh >> /home/ebossteam/RML/ojjo/salidadelnode.txt ${checkcontroller.ip} ${checkcontroller.usr} ${checkcontroller.pass} `, (error, stdout, stderr) => {
+        if (error) {
+        console.error(`error: ${error.message}`);
+        res.json({msg:`The controller information is: ${error.message}`})
+        return error.message;
+        }
+        if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        res.json({msg:`The controller information is: ${stderr}`})
+        return stderr; 
+        }
+        const versiones = {
+
+        }
+        
+        const Current = {
+            ACE3D:'-',
+            EPS:'-',
+            Txcpay:'-',
+            TxcpayPINPAD:'-',
+            ACE3DCommon:'-'
+        }
+        const Maintenance = {
+            type: 'Maintenance',
+            ACE3D:'-',
+            EPS:'-',
+            Txcpay:'-',
+            TxcpayPINPAD:'-',
+            ACE3DCommon:'-'
+        }
+        const Backup = {
+            type: 'Backup',
+            ACE3D:'-',
+            EPS:'-',
+            Txcpay:'-',
+            TxcpayPINPAD:'-',
+            ACE3DCommon:'-'
+        }
+        console.log(`stdout: \n${stdout}`);
+        stdout.indexOf('TYPE1') != -1 ? Current.ACE3D=stdout.substring(stdout.indexOf('TYPE1')+6,stdout.indexOf('TYPE1')+16):  Current.ACE3D='None'; 
+        stdout.indexOf('TYPE2') != -1 ? Maintenance.ACE3D=stdout.substring(stdout.indexOf('TYPE2')+6,stdout.indexOf('TYPE2')+16): Maintenance.ACE3D='None'; 
+        stdout.indexOf('TYPE3') != -1 ? Backup.ACE3D=stdout.substring(stdout.indexOf('TYPE3')+6,stdout.indexOf('TYPE3')+16): Backup.ACE3D='None'; 
+        versiones.Current=Current
+        versiones.Maintenance=Maintenance
+        versiones.Backup=Backup
+        // console.log(versiones)
+        res.json({versiones})
+        return stdout; 
+    });
+
+}
+export default RML;
--- Back-End/routes/api.js
+++ Back-End/routes/api.js
@@ -1,9 +1,11 @@
 import  express  from "express";
+import RML from "../controllers/RML.js";
 import API from '../controllers/scripts.js'
 
 
 const router = express.Router()
 //Aqui a tra vez del router se llama a las variables importadas del controlador que indican la vista en pug
 router.post('/',API)
+router.post('/ML',RML)
 
 export default router;
