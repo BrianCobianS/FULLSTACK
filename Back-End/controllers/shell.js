443893c5b6156ecaf2bdf693f9da20acdb1c297b
brian.cobian <brian.cobian@toshibagcs.com>
2022-12-16 13:22:40 -0600

    Haciendo eps opcional y corrigiendo errores

--- Back-End/controllers/shell.js
+++ Back-End/controllers/shell.js
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 function shel(Controlador){
    let Tcxpay =0
    let EPS =0
-   Controlador.Tcxpay != 0 ? Tcxpay = 1 : Tcxpay = 0
+   Controlador.TCxpay != 0 ? Tcxpay = 1 : Tcxpay = 0
    Controlador.nivelEPS != 0 ? EPS = 1 : EPS = 0
    exec(`python3  /home/ebossteam/UnattendedInstallation/FULLSTACK/Back-End/public/Inventories/now.py ${Controlador.ip} ${Controlador.usr} ${Controlador.pass} ${Controlador.opc} ${Controlador.ACE3Dcomple} ${Controlador.nivel} ${Controlador.ASM} ${Controlador.fecha} ${Controlador.version} ${Controlador.email} ${EPS} ${Controlador.ACE4Dcomple} ${Tcxpay} ${Controlador.ACE6Dcomple}  ${Controlador.TCxpaycommon} ${Controlador.TCxpayPinPad} `, (error, stdout, stderr) => {
       if (error) {
