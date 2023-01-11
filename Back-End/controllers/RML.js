b0d54b9b4a0bbf8d37c88381946999b484a84747
brian.cobian <brian.cobian@toshibagcs.com>
2023-01-11 11:26:04 -0600

    Change RML

--- Back-End/controllers/RML.js
+++ Back-End/controllers/RML.js
@@ -29,42 +29,50 @@ const RML = (req,res) =>{
             res.json({msg:`The controller information is: ${stderr}`})
             return stderr; 
             }
-            const versiones = {
 
-            }
-            
-            const Current = {
-                ACE3D:'-',
-                EPS:'-',
-                Txcpay:'-',
-                TxcpayPINPAD:'-',
-                ACE3DCommon:'-'
-            }
-            const Maintenance = {
-                type: 'Maintenance',
-                ACE3D:'-',
-                EPS:'-',
-                Txcpay:'-',
-                TxcpayPINPAD:'-',
-                ACE3DCommon:'-'
-            }
-            const Backup = {
-                type: 'Backup',
-                ACE3D:'-',
-                EPS:'-',
-                Txcpay:'-',
-                TxcpayPINPAD:'-',
-                ACE3DCommon:'-'
-            }
             console.log(`stdout: \n${stdout}`);
-            stdout.indexOf('TYPE1') != -1 ? Current.ACE3D=stdout.substring(stdout.indexOf('TYPE1')+6,stdout.indexOf('TYPE1')+16):  Current.ACE3D='None'; 
-            stdout.indexOf('TYPE2') != -1 ? Maintenance.ACE3D=stdout.substring(stdout.indexOf('TYPE2')+6,stdout.indexOf('TYPE2')+16): Maintenance.ACE3D='None'; 
-            stdout.indexOf('TYPE3') != -1 ? Backup.ACE3D=stdout.substring(stdout.indexOf('TYPE3')+6,stdout.indexOf('TYPE3')+16): Backup.ACE3D='None'; 
-            versiones.Current=Current
-            versiones.Maintenance=Maintenance
-            versiones.Backup=Backup
             // console.log(stdout.indexOf('UNREACHABLE!'))
             if (stdout.indexOf('UNREACHABLE!') == -1){
+
+                const versiones = {
+
+                }
+                const current = {
+                    Product: stdout.substring(stdout.indexOf("CProduct")+11,stdout.indexOf("8C")),
+                    PID: stdout.substring(stdout.indexOf("CPID")+7,stdout.indexOf("1C")),
+                    SP_Build: stdout.substring(stdout.indexOf("CSP/Build ")+12,stdout.indexOf("2C")),
+                    Base_level: stdout.substring(stdout.indexOf("CBase level")+14,stdout.indexOf("3C")),
+                    DateApplied: stdout.substring(stdout.indexOf("CDate applied")+16,stdout.indexOf("4C")),
+                    PTF: stdout.substring(stdout.indexOf("CPTF")+7,stdout.indexOf("5C")),
+                    Release: stdout.substring(stdout.indexOf("CRelease")+11,stdout.indexOf("7C")),
+                    Emergencyfix: stdout.substring(stdout.indexOf("CEmergency Fix")+17,stdout.indexOf("9C"))
+                }
+                versiones.push(current)
+                const maintenance = {
+                    PID: "-" ,
+                    SP_Build: "-" ,
+                    Base_level: "-" ,
+                    DateApplied: "-" ,
+                    PTF: "-" ,
+                    Release: "-" ,
+                    Emergencyfix: "-" 
+                }
+        
+                if(stdout.indexOf('No hay mantenimineto') == -1){
+                maintenance.Product= stdout.substring(stdout.indexOf("MProduct")+11,stdout.indexOf("8M")),
+                maintenance.PID = stdout.substring(stdout.indexOf("MPID")+7,stdout.indexOf("1M")),
+                maintenance.SP_Build = stdout.substring(stdout.indexOf("MSP/Build ")+12,stdout.indexOf("2M")),
+                maintenance.Base_level = stdout.substring(stdout.indexOf("MBase level")+14,stdout.indexOf("3M")),
+                maintenance.DateApplied = stdout.substring(stdout.indexOf("MDate applied")+16,stdout.indexOf("4M")),
+                maintenance.PTF = stdout.substring(stdout.indexOf("MPTF")+7,stdout.indexOf("5M")),
+                maintenance.Release = stdout.substring(stdout.indexOf("MRelease")+11,stdout.indexOf("7M")),
+                maintenance.Emergencyfix = stdout.substring(stdout.indexOf("MEmergency Fix")+17,stdout.indexOf("9M"))
+                }
+
+                versiones.push(maintenance)
+                console.log(current)
+                console.log(maintenance)
+                
                 res.json({versiones})
             }else{
                 res.json({error:`The controller ${checkcontroller.ip} can't be reach`})
